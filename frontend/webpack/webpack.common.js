92691b168934c5d33f31c6f1ea63b596b5778de7
lucabit73 <luca.querci@4books.com>
2022-11-25 18:31:16 +0100

    split vendor, but we don't need to use it, in app.js we have all we need

--- frontend/webpack/webpack.common.js
+++ frontend/webpack/webpack.common.js
@@ -16,7 +16,21 @@ module.exports = {
   optimization: {
     splitChunks: {
       chunks: 'all',
-      name: 'vendors',
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1];
+  
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace("@", "")}`;
+          }
+        },
+      }
     },
   },
   plugins: [
