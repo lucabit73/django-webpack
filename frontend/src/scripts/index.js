3dddb7c04febb6f3897613ad100e6099d6b074f2
lucabit73 <luca.querci@4books.com>
2022-11-28 09:50:14 +0100

    use splitted vendor files, we need it to work

--- frontend/src/scripts/index.js
+++ frontend/src/scripts/index.js
@@ -1,12 +1,12 @@
-// import '../styles/index.scss';
-// asdadwer
+import '../styles/index.scss';
+
 console.log('webpack starterkit au');
 
-// import $ from "jquery/dist/jquery.slim";
-// import "bootstrap/dist/js/bootstrap.bundle";
+import $ from "jquery/dist/jquery.slim";
+import "bootstrap/dist/js/bootstrap.bundle";
 
 $(document).ready(function () {
-    const message = 'Hello, World!';
-    console.log(message)
+  const message = 'Hello, World!';
+  console.log(message)
   window.console.log("dom ready");
 });
